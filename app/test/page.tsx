ecfbf69fdf1f9b733d7870a8867f6b57be3c5b69
Sudip Kundu <sudip2003kundu@gmail.com>
2024-04-22 22:47:59 +0530

    payment

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,47 +2,19 @@
 import React from "react";
 import { loadStripe, Stripe } from '@stripe/stripe-js';
 
-interface AddressDetails {
-    line1: string;
-    city: string;
-    state: string;
-    postal_code: string;
-    country: string;
-  }
-  
-  interface CustomerDetails {
-    name: string;
-    address: AddressDetails;
-  }
-
 
 const Home: React.FC = () => {
     const handlePayment = async () => {
-        const customerDetails: CustomerDetails = {
-            name: 'Sudip Kundu',
-            address: {
-              line1: "Brindabanpurbarsal",
-              city: "Bankura",
-              state: "West Bengal",
-              postal_code: "722142",
-              country: "India",
-            },
-          };
-          
-          const addressString = `${customerDetails.address.line1}, ${customerDetails.address.city}, ${customerDetails.address.state}, ${customerDetails.address.postal_code}, ${customerDetails.address.country}`;
+
 
         try {
-            const stripe: Stripe | null = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PK!);
+            const stripe: Stripe | null = await loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_PK}`);
             if (!stripe) {
                 throw new Error('Failed to load Stripe');
             }
 
             const body = {
                 course: "Placement Cse",
-                customerDetails: {
-                    name: customerDetails.name,
-                    address: addressString,
-                },
             };
 
             const headers = {
@@ -63,8 +35,6 @@ const Home: React.FC = () => {
             const result = await stripe.redirectToCheckout({
                 sessionId: session.id,
             });
-
-            console.log(result);
         } catch (error) {
             console.error('Error handling payment:', error);
         }
