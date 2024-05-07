3976e5430f0b0ad49cac1a53a95d5e7345128eb6
Mukesh Kr. Gupta <113277711+Mukesh015@users.noreply.github.com>
2024-05-07 18:50:12 +0530

    solving..

--- configurations/apollo/client.tsx
+++ configurations/apollo/client.tsx
@@ -10,7 +10,7 @@ import {
 
 function makeClient() {
   const httpLink = new HttpLink({
-    uri: `${process.env.NEXT_PUBLIC_FIREBASE_SERVER_DOMAIN}/graphql`,
+    uri: "https://learntube-server.onrender.com/graphql",
   });
 
   return new NextSSRApolloClient({
