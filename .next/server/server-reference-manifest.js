30b3e7c7c690219a737c9d349f77f3ae334fe8dd
Sudip Kundu <sudip2003kundu@gmail.com>
2024-05-19 04:03:22 +0530

    front end deploy

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"Dx9Yd3WBxXBD8Ftzdt5IYEEz023v3GmZkUzhZ9PIIP8=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"pd5hqk+rhKUZF4NpLYXYRY0H/KKbSeDbhRXC31728pk=\"}"
